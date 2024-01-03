0b1058d3388efccfb3d85463ddedcc51617f0b47
Moshiur Rahman Amit <moshiur.amit@gmail.com>
2024-01-03 22:45:15 +0600

    update static pages

--- src/app/store/about/page.js
+++ src/app/store/about/page.js
@@ -1,7 +1,12 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h2 className="text-3xl">About us</h2>
+      <p className="text-xl text-zinc-600 pt-2">
+        You can buy or rent a book. Also you can sell your own book. 
+      </p>
+    </div>
   )
 }
 
--- src/app/store/communities/page.js
+++ src/app/store/communities/page.js
@@ -1,7 +1,10 @@
 
 const Communities = () => {
   return (
-    <div>Communities</div>
+    <div><h2 className="text-3xl">Communities</h2>
+    <p className="text-xl text-zinc-600 pt-2">
+      You can buy or rent a book. Also you can sell your own book. 
+    </p></div>
   )
 }
 
